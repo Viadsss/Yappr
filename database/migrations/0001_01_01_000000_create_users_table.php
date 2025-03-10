79688843fe4fb4e92efde68b79d445c120cc8cd9
John Paul <113358424+Viadsss@users.noreply.github.com>
2025-03-10 16:56:00 +0800

    feat: rename avatar_url column to avatar in users table migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration {
             $table->string('name');
             $table->string('email')->unique();
             $table->string('gender')->default('unspecified');
-            $table->string('avatar_url')->nullable();
+            $table->string('avatar')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
