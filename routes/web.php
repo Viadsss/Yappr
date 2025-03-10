b539ef772c875bbcfb1f8a04c9e1be63a1dae903
John Paul <113358424+Viadsss@users.noreply.github.com>
2025-03-10 16:55:24 +0800

    feat: update routes to include authentication and session management

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,27 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\RegisteredUser;
+use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+})->name('index');
+
+Route::get('/yaps', [PostController::class, 'index'])
+    ->middleware('auth')
+    ->name('yaps.index');
+
+
+Route::middleware('guest')->group(function () {
+    Route::get('/register', [RegisteredUser::class, 'create'])->name('register.create');
+    Route::post('/register', [RegisteredUser::class, 'store'])->name('register.store');
+
+    Route::get('/login', [SessionController::class, 'create'])->name('login');
+    Route::post('/login', [SessionController::class, 'store'])->name('session.store');
 });
 
-Route::get('/test', function () {
-    return "<h1>Test</h1>";
-});
+Route::delete('/logout', [SessionController::class, 'destroy'])
+    ->middleware('auth')
+    ->name('session.destroy');
