60c500b05d6fc0ecd0a871fcb1cbcfc76e413938
John Paul <113358424+Viadsss@users.noreply.github.com>
2025-03-10 16:57:30 +0800

    feat: add RegisterRequest for user registration validation with gender and avatar fields

---
+++ app/Http/Requests/RegisterRequest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Password;
+use Str;
+
+class RegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:50'],
+            'email' => ['required', 'string', 'email:strict,dns', 'max:255', 'unique:users,email'],
+            'gender' => ['required', 'in:male,female,others,unspecified'],
+            'avatar' => ['required', 'image', 'mimes:jpg,jpeg,png', 'max:2048'],
+            'password' => ['required', Password::default(), 'confirmed']
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'avatar.max' => 'Avatar must be 2MB or smaller .'
+        ];
+    }
+
+    protected function prepareForValidation()
+    {
+        $this->merge([
+            'email' => Str::lower($this->email)
+        ]);
+    }
+}
