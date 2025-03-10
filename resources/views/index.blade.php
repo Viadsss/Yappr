95ea94a75911aefe72f5033271bff71e4e75fadf
John Paul <113358424+Viadsss@users.noreply.github.com>
2025-03-10 16:56:10 +0800

    feat: add index view with links for viewing posts and user authentication

---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+<x-layout :haveHeader="false" :haveFooter="false">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, accusantium, molestias reprehenderit voluptatum
+    officiis illo laboriosam maiores alias inventore in temporibus aspernatur? Repellat corporis quis nulla et
+    necessitatibus ea cupiditate!
+    Dolor culpa animi laborum, excepturi magni velit similique nulla sed doloribus facilis beatae odit saepe adipisci ut
+    maxime! Error perferendis cumque aliquam aperiam aliquid molestias ullam reprehenderit sed. Dolorem, deserunt?
+    Dolore cupiditate voluptatem voluptatum, provident reprehenderit magnam quaerat modi nostrum est officiis. Dolor
+    sapiente nostrum debitis rerum odio ad autem voluptate ea. Temporibus sit molestiae consectetur a facilis aliquid.
+    Cumque!
+
+    <br />
+    <br />
+    <a href="/yaps">View Yaps</a>
+    <a href="/login">Login</a>
+    <a href="/register">Register</a>
+    <input type="url" name="avatar" placeholder="Avatar URL (optional)">
+</x-layout>
