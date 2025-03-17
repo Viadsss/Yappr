963cba32302ff86c4fe707032e4b3e3d2a819bbc
John Paul <113358424+Viadsss@users.noreply.github.com>
2025-03-17 23:18:23 +0800

    feat: add authentication links and dropdown menu to header component

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,5 +1,6 @@
 <header class="bg-white">
     <div class="mx-auto max-w-screen-2xl py-2">
+
         <div class="flex items-center justify-between h-16 px-4">
             <div>
                 <a href="/" class="flex items-center gap-x-2">
@@ -9,14 +10,54 @@
                             class="text-indigo-600">.</span></span>
                 </a>
             </div>
-            <button id="menu-toggle" class="inline-flex items-center px-3 py-2 cursor-pointer gap-x-2">
-                <i id="icon-bars" class="ti ti-menu-2 text-3xl p-1 rounded"></i>
-                <i id="icon-close" class="ti ti-x text-3xl hidden bg-gray-100 p-1 rounded"></i>
-            </button>
+
+            @guest
+                <div class="space-x-4">
+                    <x-button variant="secondary" :link="true" href="{{ route('login') }}">Log In</x-button>
+                    <x-button variant="primary" :link="true" href="{{ route('register') }}">Sign Up</x-button>
+                </div>
+            @endguest
+
+            @auth
+                <div class="hidden sm:flex sm:items-center sm:ms-6">
+                    <x-dropdown align="right" width="64">
+                        <x-slot:trigger>
+                            <div class="flex px-3 items-center gap-x-4">
+                                <img src="{{ asset(auth()->user()->avatar) }}" class="w-10 h-10 rounded-full" />
+                                <p>{{ auth()->user()->name }}</p>
+                                <i class="ti ti-chevron-down"></i>
+                            </div>
+                        </x-slot:trigger>
+
+                        <x-slot:content>
+                            <x-dropdown-link href="/profile/edit">
+                                <i class="ti ti-user"></i>
+                                User
+                            </x-dropdown-link>
+
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+                                @method('DELETE')
+
+                                <x-dropdown-link :href="route('logout')"
+                                    onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+                                    <i class="ti ti-logout"></i>
+                                    Log Out
+                                </x-dropdown-link>
+                            </form>
+                        </x-slot:content>
+                    </x-dropdown>
+                </div>
+                <button id="menu-toggle" class="inline-flex items-center px-3 py-2 cursor-pointer gap-x-2 sm:hidden">
+                    <i id="icon-bars" class="ti ti-menu-2 text-3xl p-1 rounded sm:hidden"></i>
+                    <i id="icon-close" class="ti ti-x text-3xl hidden bg-gray-100 p-1 rounded sm:block"></i>
+                </button>
+            @endauth
         </div>
 
         @auth
-            <div id="menu" class="px-4 hidden">
+            <div id="menu" class="px-4 hidden sm:hidden">
                 <div class="flex py-3 items-center gap-x-2 border-y border-gray-100">
                     <img src="{{ asset(auth()->user()->avatar) }}" class="w-12 h-12 rounded-full" />
                     <div>
