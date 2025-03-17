2ca5223d4cffccd9e44d9b43f3be3b306f698c4c
John Paul <113358424+Viadsss@users.noreply.github.com>
2025-03-17 21:11:41 +0800

    feat: add header and footer components to the Yaps index view

--- resources/views/yaps/index.blade.php
+++ resources/views/yaps/index.blade.php
@@ -13,15 +13,24 @@
 
 @extends('layouts.app')
 @section('title', 'Yaps | Yappr')
+
+@section('header')
+    <x-header />
+@endsection
+
 @section('content')
     <div>
         <h1>Helloo from Index Yaps</h1>
         @foreach ($posts as $post)
             <div class="px-4 py-2">
-                <img src="{{ $post->thumbnail_url }}" />
-                <h3>{{ $post->title }}</h3>
+                {{-- <img src="{{ $post->thumbnail_url }}" /> --}}
+                {{-- <h3>{{ $post->title }}</h3> --}}
                 <h4>{{ $post->user->name }}</h4>
             </div>
         @endforeach
     </div>
+@endsection
+
+@section('footer')
+    <x-footer />
 @endsection
